9d2bda9cc7c1616d5fc25fea2b811e3608788670
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-09-01 00:31:20 -0600

    added more data

--- api/index.js
+++ api/index.js
@@ -25,50 +25,101 @@ export default async function getWeather(req, res) {
 
             // declare variables
             // TODO: finish getting all of these variables from scraping data
-            let airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, isSunUp, latitude, longitude, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, time, timezone, uvIndex, visibility, windDirection, windSpeed;
+            let airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, uvIndex, visibility, windDirection, windSpeed;
 
-            airQualityIndex = parseInt(htmlDocument.window.document.querySelector("[data-testid='AirQualityCard']").querySelector("[data-testid='DonutChartValue']").innerHTML, 10);
+            // helper functions for getting elements
+            const getElementFromTestId = (testId, parent) => {
+                parent ??= htmlDocument.window.document;
+
+                return parent.querySelector(`[data-testid="${testId}"]`);
+            }
+
+            const getAllElementsFromTestId = (testId, parent) => {
+                parent ??= htmlDocument.window.document;
+                return parent.querySelectorAll(`[data-testid="${testId}"]`);
+            }
+
+            // air quality index
+            airQualityIndex = parseInt(getElementFromTestId("DonutChartValue", getElementFromTestId("AirQualityCard")).innerHTML, 10);
+
+            // city
+            city = getElementFromTestId("CurrentConditionsContainer").getElementsByTagName("h1")[0].innerText;
+
+            // description
+            description = getElementFromTestId("wxPhrase").innerText;
+
+            // feels like
+            description = parseInt(getElementFromTestId("TemperatureValue", getElementFromTestId("FeelsLikeSection")).innerText, 10);
+
+            // rain chance
+            rainChance = parseInt(getElementFromTestId("SegmentPrecipPercentage", getElementFromTestId("DailyWeatherModule")).innerText.split("\n")[1], 10) / 100;
+
+            // sunrise
+            sunrise = getElementFromTestId("SunriseValue").innerText;
+
+            // sunset
+            sunset = getElementFromTestId("SunsetValue").innerText;
+
+            // temperature, high, low
+            let temperatureElements = getAllElementsFromTestId("TemperatureValue", getElementFromTestId("CurrentConditionsContainer"));
+
+            temperature = parseInt(temperatureElements[0].innerText);
+            high = parseInt(temperatureElements[1].innerText);
+            low = parseInt(temperatureElements[2].innerText);
 
             // today's details WeatherDetailsLabels
-            const todaysDetailsLabels = htmlDocument.window.document.querySelector("[data-testid='TodaysDetailsModule']").querySelectorAll("[data-testid='WeatherDetailsLabel']");
+            const todaysDetailsLabels = getAllElementsFromTestId("WeatherDetailsLabel", getElementFromTestId("TodaysDetailsModule"));
 
             // loop through labels
             for (let i = 0; i < todaysDetailsLabels.length; i++) {
                 // get sibling wxdata element
-                let valueElement = todaysDetailsLabels[i].parentNode.querySelector("[data-testid='wxData']");
+                const valueElement = getElementFromTestId("wxData", todaysDetailsLabels[i].parentNode);
 
                 switch (todaysDetailsLabels[i].innerText) {
-                    case "Wind":
+                    case "Humidity": {
 
+                        humidity = parseInt(valueElement.innerText, 10) / 100;
                         break;
+                    }
 
-                    case "Humidity":
-
+                    case "Pressure": {
+                        pressure = parseFloat(valueElement.innerText, 10)
                         break;
+                    }
 
-                    case "Dew Point":
-
+                    case "Visibility": {
+                        visibility = parseInt(valueElement.innerText, 10);
                         break;
+                    }
 
-                    case "Pressure":
+                    case "Wind": {
+                        // wind speed
+                        windSpeed = parseInt(valueElement.innerText, 10);
 
+                        // wind direction
+                        windDirectionElement = getElementFromTestId("Icon", valueElement);
+                        windDirection = parseFloat(windDirectionElement.style.transform.substring(7));
                         break;
+                    }
 
-                    case "UV Index":
-
+                    case "Dew Point": {
+                        dewPoint = parseInt(valueElement.innerText, 10);
                         break;
+                    }
 
-                    case "Visibility":
-                        visibility = parseInt(valueElement.innerText);
+                    case "UV Index": {
+                        uvIndex = parseInt(valueElement.innerText, 10)
                         break;
+                    }
 
-                    case "Moon Phase":
+                    case "Moon Phase": {
                         moonPhase = valueElement.innerText;
                         break;
+                    }
                 }
             }
 
             // send response json
-            res.status(200).json({ airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, isSunUp, latitude, longitude, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, time, timezone, uvIndex, visibility, windDirection, windSpeed });
+            res.status(200).json({ airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, latitude, longitude, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, uvIndex, visibility, windDirection, windSpeed });
         });
 }
