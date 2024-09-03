e4c2d9b077c5e515db00f7621bc2dc7be152b550
ArjhanToteck <38510221+ArjhanToteck@users.noreply.github.com>
2024-09-02 23:42:06 -0600

    formatting, codeblock work

--- api/index.js
+++ api/index.js
@@ -1,138 +1,138 @@
 const { JSDOM } = require("jsdom");
 
 export default async function getWeather(req, res) {
-    const { searchParams } = new URL(process.env.NEXT_PUBLIC_PROCESSING_SERVER + req.url);
+	const { searchParams } = new URL(process.env.NEXT_PUBLIC_PROCESSING_SERVER + req.url);
 
-    const latitude = searchParams.get("latitude");
-    const longitude = searchParams.get("longitude");
+	const latitude = searchParams.get("latitude");
+	const longitude = searchParams.get("longitude");
 
-    if (!latitude || !longitude) {
-        res.status(400).send("Latitude and longitude not included or not formatted correctly.");
-    }
+	if (!latitude || !longitude) {
+		res.status(400).send("Latitude and longitude not included or not formatted correctly.");
+	}
 
-    await fetch(`https://weather.com/weather/today/l/${latitude},${longitude}`)
-        .then((response) => {
-            if (!response.ok) {
-                res.status(502).send("Bad gateway. Error reaching weather servers.");
-                return;
-            }
+	await fetch(`https://weather.com/weather/today/l/${latitude},${longitude}`)
+		.then((response) => {
+			if (!response.ok) {
+				res.status(502).send("Bad gateway. Error reaching weather servers.");
+				return;
+			}
 
-            return response.text();
-        }).then((rawHtml) => {
-            // parse html
-            const htmlDocument = new JSDOM(rawHtml);
+			return response.text();
+		}).then((rawHtml) => {
+			// parse html
+			const htmlDocument = new JSDOM(rawHtml);
 
-            // declare variables
-            // TODO: finish getting all of these variables from scraping data
-            let airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, timeUpdated, timezone, uvIndex, visibility, windDirection, windSpeed;
+			// declare variables
+			// TODO: finish getting all of these variables from scraping data
+			let airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, timeUpdated, timezone, uvIndex, visibility, windDirection, windSpeed;
 
-            // helper functions for getting elements
-            const getElementFromTestId = (testId, parent = htmlDocument.window.document) => {
-                return parent.querySelector(`[data-testid="${testId}"]`);
-            };
+			// helper functions for getting elements
+			const getElementFromTestId = (testId, parent = htmlDocument.window.document) => {
+				return parent.querySelector(`[data-testid="${testId}"]`);
+			};
 
-            const getAllElementsFromTestId = (testId, parent = htmlDocument.window.document) => {
-                return parent.querySelectorAll(`[data-testid="${testId}"]`);
-            };
+			const getAllElementsFromTestId = (testId, parent = htmlDocument.window.document) => {
+				return parent.querySelectorAll(`[data-testid="${testId}"]`);
+			};
 
-            // air quality index
-            airQualityIndex = parseInt(getElementFromTestId("DonutChartValue", getElementFromTestId("AirQualityCard")).textContent, 10);
+			// air quality index
+			airQualityIndex = parseInt(getElementFromTestId("DonutChartValue", getElementFromTestId("AirQualityCard")).textContent, 10);
 
-            // city
-            city = getElementFromTestId("CurrentConditionsContainer").getElementsByTagName("h1")[0].textContent;
+			// city
+			city = getElementFromTestId("CurrentConditionsContainer").getElementsByTagName("h1")[0].textContent;
 
-            // description
-            description = getElementFromTestId("wxPhrase").textContent;
+			// description
+			description = getElementFromTestId("wxPhrase").textContent;
 
-            // feels like
-            feelsLike = parseInt(getElementFromTestId("TemperatureValue", getElementFromTestId("FeelsLikeSection")).textContent, 10);
+			// feels like
+			feelsLike = parseInt(getElementFromTestId("TemperatureValue", getElementFromTestId("FeelsLikeSection")).textContent, 10);
 
-            // rain chance
+			// rain chance
 
-            // get first rain chance element
-            const firstRainElement = getElementFromTestId("SegmentPrecipPercentage", getElementFromTestId("DailyWeatherModule"));
+			// get first rain chance element
+			const firstRainElement = getElementFromTestId("SegmentPrecipPercentage", getElementFromTestId("DailyWeatherModule"));
 
-            // extract number, parse, and convert from percentage to decimal
-            rainChance = parseInt(firstRainElement.textContent.match(/\d+/)[0], 10) / 100;
+			// extract number, parse, and convert from percentage to decimal
+			rainChance = parseInt(firstRainElement.textContent.match(/\d+/)[0], 10) / 100;
 
-            // date stuff
-            const timeStringToDate = (timeString) => {
-                return new Date(new Date().toDateString() + " " + timeString);
-            };
+			// date stuff
+			const timeStringToDate = (timeString) => {
+				return new Date(new Date().toDateString() + " " + timeString);
+			};
 
-            // time updated and timezone
-            const timeUpdatedString = getElementFromTestId("CurrentConditionsContainer").getElementsByTagName("span")[0].textContent;
-            const splitTimeUpdatedString = timeUpdatedString.split(" ");
+			// time updated and timezone
+			const timeUpdatedString = getElementFromTestId("CurrentConditionsContainer").getElementsByTagName("span")[0].textContent;
+			const splitTimeUpdatedString = timeUpdatedString.split(" ");
 
-            timezone = splitTimeUpdatedString[splitTimeUpdatedString.length - 1];
-            timeUpdated = timeStringToDate(timeUpdatedString.split("As of ")[1]).toISOString();
+			timezone = splitTimeUpdatedString[splitTimeUpdatedString.length - 1];
+			timeUpdated = timeStringToDate(timeUpdatedString.split("As of ")[1]).toISOString();
 
-            // sunrise
-            sunrise = timeStringToDate(getElementFromTestId("SunriseValue").textContent.substring(8) + " " + timezone).toISOString();
+			// sunrise
+			sunrise = timeStringToDate(getElementFromTestId("SunriseValue").textContent.substring(8) + " " + timezone).toISOString();
 
-            // sunset
-            sunset = timeStringToDate(getElementFromTestId("SunsetValue").textContent.substring(6) + " " + timezone).toISOString();
+			// sunset
+			sunset = timeStringToDate(getElementFromTestId("SunsetValue").textContent.substring(6) + " " + timezone).toISOString();
 
-            // temperature, high, low
-            const temperatureElements = getAllElementsFromTestId("TemperatureValue", getElementFromTestId("CurrentConditionsContainer"));
+			// temperature, high, low
+			const temperatureElements = getAllElementsFromTestId("TemperatureValue", getElementFromTestId("CurrentConditionsContainer"));
 
-            temperature = parseInt(temperatureElements[0].textContent);
-            high = parseInt(temperatureElements[1].textContent);
-            low = parseInt(temperatureElements[2].textContent);
+			temperature = parseInt(temperatureElements[0].textContent);
+			high = parseInt(temperatureElements[1].textContent);
+			low = parseInt(temperatureElements[2].textContent);
 
-            // today's details WeatherDetailsLabels
-            const todaysDetailsLabels = getAllElementsFromTestId("WeatherDetailsLabel", getElementFromTestId("TodaysDetailsModule"));
+			// today's details WeatherDetailsLabels
+			const todaysDetailsLabels = getAllElementsFromTestId("WeatherDetailsLabel", getElementFromTestId("TodaysDetailsModule"));
 
-            // loop through labels
-            for (let i = 0; i < todaysDetailsLabels.length; i++) {
-                // get sibling wxdata element
-                const valueElement = getElementFromTestId("wxData", todaysDetailsLabels[i].parentNode);
+			// loop through labels
+			for (let i = 0; i < todaysDetailsLabels.length; i++) {
+				// get sibling wxdata element
+				const valueElement = getElementFromTestId("wxData", todaysDetailsLabels[i].parentNode);
 
-                switch (todaysDetailsLabels[i].textContent) {
-                    case "Humidity": {
+				switch (todaysDetailsLabels[i].textContent) {
+					case "Humidity": {
 
-                        humidity = parseInt(valueElement.textContent, 10) / 100;
-                        break;
-                    }
+						humidity = parseInt(valueElement.textContent, 10) / 100;
+						break;
+					}
 
-                    case "Pressure": {
-                        pressure = parseFloat(valueElement.textContent.substring(8), 10);
-                        break;
-                    }
+					case "Pressure": {
+						pressure = parseFloat(valueElement.textContent.substring(8), 10);
+						break;
+					}
 
-                    case "Visibility": {
-                        visibility = parseInt(valueElement.textContent, 10);
-                        break;
-                    }
+					case "Visibility": {
+						visibility = parseInt(valueElement.textContent, 10);
+						break;
+					}
 
-                    case "Wind": {
-                        // wind speed
-                        windSpeed = parseInt(valueElement.textContent.substring(14), 10);
+					case "Wind": {
+						// wind speed
+						windSpeed = parseInt(valueElement.textContent.substring(14), 10);
 
-                        // wind direction
-                        let windDirectionElement = getElementFromTestId("Icon", valueElement);
-                        windDirection = parseFloat(windDirectionElement.style.transform.substring(7));
-                        break;
-                    }
+						// wind direction
+						let windDirectionElement = getElementFromTestId("Icon", valueElement);
+						windDirection = parseFloat(windDirectionElement.style.transform.substring(7));
+						break;
+					}
 
-                    case "Dew Point": {
-                        dewPoint = parseInt(valueElement.textContent, 10);
-                        break;
-                    }
+					case "Dew Point": {
+						dewPoint = parseInt(valueElement.textContent, 10);
+						break;
+					}
 
-                    case "UV Index": {
-                        uvIndex = parseInt(valueElement.textContent, 10)
-                        break;
-                    }
+					case "UV Index": {
+						uvIndex = parseInt(valueElement.textContent, 10)
+						break;
+					}
 
-                    case "Moon Phase": {
-                        moonPhase = valueElement.textContent;
-                        break;
-                    }
-                }
-            }
+					case "Moon Phase": {
+						moonPhase = valueElement.textContent;
+						break;
+					}
+				}
+			}
 
-            // send response json
-            res.status(200).json({ airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, latitude, longitude, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, timeUpdated, timezone, uvIndex, visibility, windDirection, windSpeed });
-        });
+			// send response json
+			res.status(200).json({ airQualityIndex, city, description, dewPoint, feelsLike, high, humidity, latitude, longitude, low, moonPhase, pressure, rainChance, sunrise, sunset, temperature, timeUpdated, timezone, uvIndex, visibility, windDirection, windSpeed });
+		});
 }
--- app/page.js
+++ app/page.js
@@ -1,149 +1,146 @@
 import CodeBlock from "/components/CodeBlock.js";
 
 export default function page() {
-    return (
-        <main>
-            <section>
-                <h1>Cumulus</h1>
-                <p>Cumulus is a free and simple weather API without the need for an API key. I originally made this a few years ago, but it was terrible. Hopefully this works a little better.</p>
+	return (
+		<main>
+			<section>
+				<h1>Cumulus</h1>
+				<p>Cumulus is a free and simple weather API without the need for an API key. I originally made this a few years ago, but it was terrible. Hopefully this works a little better.</p>
 
-                <h2>Demo</h2>
+				<h2>Demo</h2>
 
-                <h2>Endpoint</h2>
-                <p>Simply make an HTTP request to {process.env.NEXT_PUBLIC_SOCKETIO_SERVER}/api/projects/cumulus?latitude=LATITUDE&longitude=LONGITUDE and read the data as a JSON.</p>
+				<h2>Endpoint</h2>
+				<p>Simply make an HTTP request to {process.env.NEXT_PUBLIC_SOCKETIO_SERVER}/api/projects/cumulus?latitude=LATITUDE&longitude=LONGITUDE and read the data as a JSON. Obviously, make sure to replace LATITUDE and LONGITUDE with an actual valid latitude and longitude.</p>
 
-                <p>In JavaScript, for example:</p>
+				<p>In JavaScript, for example:</p>
 
-                <CodeBlock>
-                // TODO: figure out whats going on with the indents (&#9;)
-                    fetch("{process.env.NEXT_PUBLIC_SOCKETIO_SERVER}/api/projects/cumulus?latitude=LATITUDE&longitude=LONGITUDE")
-                    <br></br>
-                    &#9;.then((response) =&gt; response.json())
-                    <br></br>
-                    &#9;.then(console.log);
-                </CodeBlock>
+				<CodeBlock code={
+					`fetch("${process.env.NEXT_PUBLIC_SOCKETIO_SERVER}/api/projects/cumulus?latitude=LATITUDE&longitude=LONGITUDE")` + "\n" +
+					`	.then((response) => response.json())` + "\n" +
+					`	.then(console.log);`
+				} />
 
-                <h2>Documentation</h2>
-                <p>Cumulus returns the following data in a JSON format. Note that the data may be outdated by a few minutes, the time at which the data was updated is available in the timeUpdated property.</p>
-                <table>
-                    <tr>
-                        <th>Property Name</th>
-                        <th>Description</th>
-                        <th>Data Type</th>
-                    </tr>
-                    <tr>
-                        <td>airQualityIndex</td>
-                        <td>A number ranging from 0 to 500 describing the amount of pollution in the air. If this value is low, it means the air quality is good. If this value is high, it means the air quality is poor.</td>
-                        <td>Integer</td>
-                    </tr>
-                    <tr>
-                        <td>city</td>
-                        <td>The name of the city/town where the weather is being observed.</td>
-                        <td>String</td>
-                    </tr>
-                    <tr>
-                        <td>description</td>
-                        <td>A basic description of the weather. Some examples include "Cloudy," "Clear," "Fair," "Mostly Cloudy," "Partly Cloudy," and "Sunny."</td>
-                        <td>String</td>
-                    </tr>
-                    <tr>
-                        <td>dewPoint</td>
-                        <td>The temperature in Fahrenheit that needs to be reached for the air to reach a humidity of 100%.</td>
-                        <td>Integer</td>
-                    </tr>
-                    <tr>
-                        <td>feelsLike</td>
-                        <td>What the temperature feels like outside due to the wind, sun, and other factors measured in Fahrenheit.</td>
-                        <td>Integer</td>
-                    </tr>
-                    <tr>
-                        <td>high</td>
-                        <td>The highest predicted temperature in Fahrenheit.</td>
-                        <td>Integer</td>
-                    </tr>
-                    <tr>
-                        <td>humidity</td>
-                        <td>A decimal number indicating the humidity from a scale of 0 to 1.</td>
-                        <td>Decimal</td>
-                    </tr>
-                    <tr>
-                        <td>latitude</td>
-                        <td>The latitude used to access the data.</td>
-                        <td>Decimal</td>
-                    </tr>
-                    <tr>
-                        <td>longitude</td>
-                        <td>The longitude used to access the data.</td>
-                        <td>Decimal</td>
-                    </tr>
-                    <tr>
-                        <td>low</td>
-                        <td>The lowest predicted temperature in Fahrenheit.</td>
-                        <td>Integer</td>
-                    </tr>
-                    <tr>
-                        <td>moonPhase</td>
-                        <td>The phase the moon is taking in the lunar cycle.</td>
-                        <td>String</td>
-                    </tr>
-                    <tr>
-                        <td>pressure</td>
-                        <td>The atmospheric pressure measured in inches.</td>
-                        <td>Decimal</td>
-                    </tr>
-                    <tr>
-                        <td>rainChance</td>
-                        <td>The chances it will rain today as a number from a scale of 0 to 1.</td>
-                        <td>Decimal</td>
-                    </tr>
-                    <tr>
-                        <td>sunrise</td>
-                        <td>The time in which the sun rises reported in an ISO date format.</td>
-                        <td>String</td>
-                    </tr>
-                    <tr>
-                        <td>sunset</td>
-                        <td>The time in which the sun sets reported in an ISO date format.</td>
-                        <td>String</td>
-                    </tr>
-                    <tr>
-                        <td>temperature</td>
-                        <td>The current temperature in Fahrenheit.</td>
-                        <td>Number</td>
-                    </tr>
-                    <tr>
-                        <td>timeUpdated</td>
-                        <td>The time at which the data was reported in in an ISO date format.</td>
-                        <td>String</td>
-                    </tr>
-                    <tr>
-                        <td>timezone</td>
-                        <td>The timezone reported in its abbreviated form. For example, "EST" or "UTC."</td>
-                        <td>String</td>
-                    </tr>
-                    <tr>
-                        <td>uvIndex</td>
-                        <td>The measurement of UV rays received from the sun in the area, reported as a number from a scale of 0 to 1.</td>
-                        <td>Number</td>
-                    </tr>
-                    <tr>
-                        <td>visibility</td>
-                        <td>The number of miles visible from a given point under the current weather conditions.</td>
-                        <td>Number</td>
-                    </tr>
-                    <tr>
-                        <td>windDirection</td>
-                        <td>The direction in which the wind blows as an angle. Note that 0 is South, 180 is North, 90 is west, and 270 is east.</td>
-                        <td>Number</td>
-                    </tr>
-                    <tr>
-                        <td>windSpeed</td>
-                        <td>The speed of the wind measured in miles per hour.</td>
-                        <td>Number</td>
-                    </tr>
-                </table>
-                <br></br>
-            </section>
-        </main>
-    );
+				<h2>Documentation</h2>
+				<p>Cumulus returns the following data in a JSON format. Note that the data may be outdated by a few minutes, the time at which the data was updated is available in the timeUpdated property.</p>
+				<table>
+					<tr>
+						<th>Property Name</th>
+						<th>Description</th>
+						<th>Data Type</th>
+					</tr>
+					<tr>
+						<td>airQualityIndex</td>
+						<td>A number ranging from 0 to 500 describing the amount of pollution in the air. If this value is low, it means the air quality is good. If this value is high, it means the air quality is poor.</td>
+						<td>Integer</td>
+					</tr>
+					<tr>
+						<td>city</td>
+						<td>The name of the city/town where the weather is being observed.</td>
+						<td>String</td>
+					</tr>
+					<tr>
+						<td>description</td>
+						<td>A basic description of the weather. Some examples include "Cloudy," "Clear," "Fair," "Mostly Cloudy," "Partly Cloudy," and "Sunny."</td>
+						<td>String</td>
+					</tr>
+					<tr>
+						<td>dewPoint</td>
+						<td>The temperature in Fahrenheit that needs to be reached for the air to reach a humidity of 100%.</td>
+						<td>Integer</td>
+					</tr>
+					<tr>
+						<td>feelsLike</td>
+						<td>What the temperature feels like outside due to the wind, sun, and other factors measured in Fahrenheit.</td>
+						<td>Integer</td>
+					</tr>
+					<tr>
+						<td>high</td>
+						<td>The highest predicted temperature in Fahrenheit.</td>
+						<td>Integer</td>
+					</tr>
+					<tr>
+						<td>humidity</td>
+						<td>A decimal number indicating the humidity from a scale of 0 to 1.</td>
+						<td>Decimal</td>
+					</tr>
+					<tr>
+						<td>latitude</td>
+						<td>The latitude used to access the data.</td>
+						<td>Decimal</td>
+					</tr>
+					<tr>
+						<td>longitude</td>
+						<td>The longitude used to access the data.</td>
+						<td>Decimal</td>
+					</tr>
+					<tr>
+						<td>low</td>
+						<td>The lowest predicted temperature in Fahrenheit.</td>
+						<td>Integer</td>
+					</tr>
+					<tr>
+						<td>moonPhase</td>
+						<td>The phase the moon is taking in the lunar cycle.</td>
+						<td>String</td>
+					</tr>
+					<tr>
+						<td>pressure</td>
+						<td>The atmospheric pressure measured in inches.</td>
+						<td>Decimal</td>
+					</tr>
+					<tr>
+						<td>rainChance</td>
+						<td>The chances it will rain today as a number from a scale of 0 to 1.</td>
+						<td>Decimal</td>
+					</tr>
+					<tr>
+						<td>sunrise</td>
+						<td>The time in which the sun rises reported in an ISO date format.</td>
+						<td>String</td>
+					</tr>
+					<tr>
+						<td>sunset</td>
+						<td>The time in which the sun sets reported in an ISO date format.</td>
+						<td>String</td>
+					</tr>
+					<tr>
+						<td>temperature</td>
+						<td>The current temperature in Fahrenheit.</td>
+						<td>Number</td>
+					</tr>
+					<tr>
+						<td>timeUpdated</td>
+						<td>The time at which the data was reported in in an ISO date format.</td>
+						<td>String</td>
+					</tr>
+					<tr>
+						<td>timezone</td>
+						<td>The timezone reported in its abbreviated form. For example, "EST" or "UTC."</td>
+						<td>String</td>
+					</tr>
+					<tr>
+						<td>uvIndex</td>
+						<td>The measurement of UV rays received from the sun in the area, reported as a number from a scale of 0 to 1.</td>
+						<td>Number</td>
+					</tr>
+					<tr>
+						<td>visibility</td>
+						<td>The number of miles visible from a given point under the current weather conditions.</td>
+						<td>Number</td>
+					</tr>
+					<tr>
+						<td>windDirection</td>
+						<td>The direction in which the wind blows as an angle. Note that 0 is South, 180 is North, 90 is west, and 270 is east.</td>
+						<td>Number</td>
+					</tr>
+					<tr>
+						<td>windSpeed</td>
+						<td>The speed of the wind measured in miles per hour.</td>
+						<td>Number</td>
+					</tr>
+				</table>
+				<br></br>
+			</section>
+		</main >
+	);
 }
